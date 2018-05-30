17d852787bb6aafbffb44b86e00e69a44aba0733
lidq <ldq0183@woozoom.net>
2018-05-30 14:41:30 +0800

    调整参数

---
+++ cp_params_xml.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp /data/work/workspace/pa/096.PA-FlightControl/build/pa3-hil_default/parameters.xml /data/work/workspace/pa/091.qgroundcontrol/src/FirmwarePlugin/PX4/PX4ParameterFactMetaData.xml
+
